6b2de7ac64b13443b05a51534eba53044965266f
AlyszaZealand <als012@edu.zealand.dk>
2026-04-20 11:33:34 +0200

    owner on cat + lougout button in the header

--- src/main/java/feedback/racekatteklubben/Model/MemberDTO.java
+++ src/main/java/feedback/racekatteklubben/Model/MemberDTO.java
@@ -1,34 +1,34 @@
-package feedback.racekatteklubben.Model;
-
-public class MemberDTO {
-
-    private int memberID;
-    private String username;
-    private String email;
-
-
-    public MemberDTO(int memberID, String username, String email) {
-        this.memberID = memberID;
-        this.username = username;
-        this.email = email;
-    }
-
-    public int getMemberID() {
-        return memberID;
-    }
-    public void setMemberID(int memberID) {
-        this.memberID = memberID;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
+//package feedback.racekatteklubben.Model;
+//
+//public class MemberDTO {
+//
+//    private int memberID;
+//    private String username;
+//    private String email;
+//
+//
+//    public MemberDTO(int memberID, String username, String email) {
+//        this.memberID = memberID;
+//        this.username = username;
+//        this.email = email;
+//    }
+//
+//    public int getMemberID() {
+//        return memberID;
+//    }
+//    public void setMemberID(int memberID) {
+//        this.memberID = memberID;
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//    public String getEmail() {
+//        return email;
+//    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//}
